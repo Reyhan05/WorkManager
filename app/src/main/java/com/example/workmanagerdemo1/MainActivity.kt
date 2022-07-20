b007b458eafb9eddcddfdf5e0d603fe4df7cbf5f
reyhankelas7bputrabukyayah <reyhanmohamad0212@gmail.com>
2022-07-20 13:13:41 +0700

    finis the work manager

--- app/src/main/java/com/example/workmanagerdemo1/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerdemo1/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.workmanagerdemo1
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.*
 import com.example.workmanagerdemo1.databinding.ActivityMainBinding
 
 
@@ -53,6 +55,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setOneTimeWorkRequest() {
+        val workManager = WorkManager.getInstance(applicationContext)
+
+        // Constraint digunakan untuk syarat worker akan berjalan apabila terpenuhi
+        val constraint = Constraints.Builder()
+
+            // Bisa berjalan ketika hp dalam keadaan ngecharge dan terhubung internet
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresCharging(true)
+            .build()
+
+        // Membuat input ke dalam worker UploaderWorker.kt
+        val data = Data.Builder()
+            .putInt(KEY_COUNT_VALUE, 125)
+            .build()
+
+        // Membuat work request
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .setConstraints(constraint)
+            .setInputData(data)
+            .build()
+
+        // Mengirimkan workrequest ke sistem
+        workManager.enqueue(uploadRequest)
+
+        // Mendapatkan status workManager
+        workManager.getWorkInfoByIdLiveData(uploadRequest.id)
+            .observe(this, {
+                binding.status.text = it.state.name
+                if (it.state.isFinished) {
+                    val data = it.outputData
+                    val message = data.getString(UploadWorker.KEY_WORKER)
+                    Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+                }
+            })
 
     }
 }
