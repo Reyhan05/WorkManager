b2cc1c18296e12522abfb4def718661710aa44e1
reyhankelas7bputrabukyayah <reyhanmohamad0212@gmail.com>
2022-07-20 10:49:59 +0700

    :tada: Initial Commit

---
+++ app/src/main/java/com/example/workmanagerdemo1/UploadWorker.kt
@@ -0,0 +1,24 @@
+package com.example.workmanagerdemo1
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+
+class UploadWorker(context: Context, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
+
+    companion object {
+        const val KEY_WORKER = "key_worker"
+    }
+
+    override fun doWork(): Result {
+        return try {
+
+
+            Result.success()
+        } catch (e: Exception) {
+            Result.failure()
+        }
+    }
+}
